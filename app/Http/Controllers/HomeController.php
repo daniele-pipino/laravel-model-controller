44fab8e38d6dd6bb35148ca4f628e82526736e58
daniele-pipino <Danielemacpipino@icloud.com>
2021-10-07 16:40:37 +0200

    creazione del model e importazione nel controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Movie;
+
 
 class HomeController extends Controller
 {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,15 @@
 <section id="home-section">
     <div class="container text-center">
         <h1 class="my-5">Ciao questa è la Home</h1>
+
+        {{-- card per i movie --}}
+        <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
     </div>
 </section>
 @endsection
