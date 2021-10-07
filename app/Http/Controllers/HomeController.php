bf33a1f84a7c39a707e8e876f5318e16c9567228
daniele-pipino <Danielemacpipino@icloud.com>
2021-10-07 16:51:56 +0200

    recupero e inserimento dei film dal databes in pagina

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $movies = Movie::all();
+        return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,20 @@
 @section('content')
 <section id="home-section">
     <div class="container text-center">
-        <h1 class="my-5">Ciao questa è la Home</h1>
+        <h1 class="my-5">Film</h1>
 
         {{-- card per i movie --}}
+        @foreach ($movies as $movie)
         <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
+          <div class="card-body">
+            <h5 class="card-title">{{$movie['title']}}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Original-Title: {{$movie['original_title']}}</h6>
+            <h6 class="card-subtitle mb-2 text-success">Nationality: {{$movie['nationality']}}</h6>
+            <p class="card-text">Uscito il:{{$movie['date']}}</p>
+            <p class="card-text text-primary">Voto: {{$movie['vote']}}</p>
           </div>
+        </div>
+        @endforeach
     </div>
 </section>
 @endsection
