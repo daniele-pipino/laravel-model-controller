82dc239f2cbbcafae4ad764f4bca4488a6d91194
daniele-pipino <Danielemacpipino@icloud.com>
2021-10-07 15:34:41 +0200

    layout base

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.main')
+@section('title', 'Home')
+@section('content')
+<section id="home-section">
+    <div class="container text-center">
+        <h1>Ciao questa è la Home</h1>
+    </div>
+</section>
+@endsection
