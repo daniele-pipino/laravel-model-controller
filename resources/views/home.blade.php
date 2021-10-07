9fa3ba4ee52e7cb1889ad96a1bf20afe2f0a46d2
daniele-pipino <Danielemacpipino@icloud.com>
2021-10-07 17:49:02 +0200

    correzione sintassi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,13 +7,13 @@
 
         {{-- card per i movie --}}
         @foreach ($movies as $movie)
-        <div class="card">
+        <div class="card my-5 w-50 mx-auto">
           <div class="card-body">
-            <h5 class="card-title">{{$movie['title']}}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Original-Title: {{$movie['original_title']}}</h6>
-            <h6 class="card-subtitle mb-2 text-success">Nationality: {{$movie['nationality']}}</h6>
-            <p class="card-text">Uscito il:{{$movie['date']}}</p>
-            <p class="card-text text-primary">Voto: {{$movie['vote']}}</p>
+            <h5 class="card-title">{{$movie->title}}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Original-Title: {{$movie->original_title}}</h6>
+            <h6 class="card-subtitle mb-2 text-success">Nationality: {{$movie->nationality}}</h6>
+            <p class="card-text">Uscito il:  {{$movie->date}}</p>
+            <p class="card-text text-primary">Voto: {{$movie->vote}}</p>
           </div>
         </div>
         @endforeach
