c56737b0cfec6ef99f61a5907f6f2701b96abc9f
daniele-pipino <Danielemacpipino@icloud.com>
2021-10-07 16:05:30 +0200

    creazione controller e reindirizzamento della home completo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <section id="home-section">
     <div class="container text-center">
-        <h1>Ciao questa è la Home</h1>
+        <h1 class="my-5">Ciao questa è la Home</h1>
     </div>
 </section>
 @endsection
